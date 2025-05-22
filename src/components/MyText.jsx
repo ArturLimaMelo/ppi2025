609a4f47825a1ff0957a6faf27d31a24ffabffe6
ArturLimaMelo <151844637+ArturLimaMelo@users.noreply.github.com>
2025-05-22 17:19:42 +0000

    22/05

--- src/components/MyText.jsx
+++ src/components/MyText.jsx
@@ -1,15 +1,17 @@
-import styles from './MyText.module.css';
+import styles from "./MyText.module.css";
 
 export function MyText() {
   return (
-    <div ClassName={styles.div}>
-      <h1 ClassName={styles.title}>Meu primeiro React App</h1>
-      <p ClassName={styles.text}>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Perferendis
-        sapiente ipsam adipisci, veritatis fugiat sed id vitae voluptas!
-        Expedita saepe, odio accusamus repellendus eaque atque excepturi iure
-        omnis sit rem!
-      </p>
+    <div className={styles.container}>
+      <div className={styles.div}>
+        <h1 className={styles.title}>Meu primeiro React App</h1>
+        <p className={styles.text}>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Perferendis
+          sapiente ipsam adipisci, veritatis fugiat sed id vitae voluptas!
+          Expedita saepe, odio accusamus repellendus eaque atque excepturi iure
+          omnis sit rem!
+        </p>
+      </div>
     </div>
   );
 }
