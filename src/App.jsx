d8154e4dda0fc20886b47ca12bff0793e8c737db
ArturLimaMelo <151844637+ArturLimaMelo@users.noreply.github.com>
2025-05-23 17:13:20 +0000

    23/05

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,22 @@ import "./styles/global.css";
 import { MyText } from "./components/MyText";
 
 export default function App() {
+  const text = [
+    { text: "Meu texto ", title: "Meu titulo " },
+    { text: "Meu texto ", title: "Meu titulo " },
+    { text: "Meu texto ", title: "Meu titulo " },
+  ]; // array de objetos com texto e título
+
   return (
     <>
-      <MyText />
-      <MyText />
-      <MyText />
+      {
+        text.map(
+          (item, index) => (
+            index++,
+            (<MyText title={item.title + index}>{item.text + index}</MyText>)
+          )
+        ) //item.text é o filho de MyText <Pai classe="">Filho</Pai>
+      }
     </>
   );
 }
-
--- src/components/MyText.jsx
+++ src/components/MyText.jsx
@@ -1,17 +1,14 @@
+import { Children } from "react";
 import styles from "./MyText.module.css";
 
-export function MyText() {
+export function MyText({ title, children }) {
+  // Desestruturação de props
   return (
     <div className={styles.container}>
       <div className={styles.div}>
-        <h1 className={styles.title}>Meu primeiro React App</h1>
-        <p className={styles.text}>
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Perferendis
-          sapiente ipsam adipisci, veritatis fugiat sed id vitae voluptas!
-          Expedita saepe, odio accusamus repellendus eaque atque excepturi iure
-          omnis sit rem!
-        </p>
+        <h1 className={styles.title}>{title}</h1>
+        <p className={styles.text}>{children}</p>
       </div>
-    </div>
+    </div> // uso de classes e filhos para inserção de texto, title = classe
   );
 }
