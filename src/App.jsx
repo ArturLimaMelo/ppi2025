eddf5f7af78f08c05e76c4ecca802665a77968c9
ArturLimaMelo <151844637+ArturLimaMelo@users.noreply.github.com>
2025-05-29 17:26:38 +0000

    29/05

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,14 @@
 import "./styles/theme.css";
 import "./styles/global.css";
 import { MyText } from "./components/MyText";
+import { MyTextList } from "./components/MyTextList";
+
 
 export default function App() {
-  const texts = [
-    { text: "Meu texto ", title: "Meu titulo " },
-    { text: "Meu texto ", title: "Meu titulo " },
-    { text: "Meu texto ", title: "Meu titulo " },
-    { text: "Meu texto ", title: "Meu titulo " },
-    { text: "Meu texto ", title: "Meu titulo " },
-  ]; // array de objetos com texto e título
 
   return (
     <>
-      {
-        texts.map(
-          (item, index) => (
-            index++,
-            (<MyText title={item.title + index}>{item.text + index}</MyText>)
-          )
-        ) //item.text é o filho de MyText <Pai classe="">Filho</Pai>
-      }
-    </> // Abrimos {} para podermos programar em JS dentro do JSX
+      <MyTextList />
+    </>
   );
 }
