db08aec5084d607aaaa287000f02fd361bed35ff
ArturLimaMelo <151844637+ArturLimaMelo@users.noreply.github.com>
2025-06-12 17:15:38 +0000

    images

--- src/components/MyGrid.jsx
+++ src/components/MyGrid.jsx
@@ -5,30 +5,35 @@ export function MyGrid() {
   return (
     <div className={styles.container}>
       <header className={styles.header1}>
-        <img src="https://www.flaticon.com/free-icon/duck_17839205?term=duck&page=1&position=52&origin=search&related_id=17839205"/>
+        <img className={styles.imageH}src="https://cdn-icons-png.flaticon.com/128/4659/4659467.png"/>
         <h1 className={styles.title}>Foco, Força, Fé</h1>
       </header>
-      <main className={styles.main}>
+      <main className={styles.main}>  
         <div className={styles.grid}>
           <div className={styles.card}>
-            <h2>Card 1</h2>
-            <p>Conteúdo do Card 1</p>
+            <img className={styles.image}src="https://picsum.photos/200/300?random=1"></img>
+            <h2 className={styles.titleC}>My Text 0</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
           </div>
           <div className={styles.card}>
-            <h2>Card 2</h2>
-            <p>Conteúdo do Card 2</p>
+            <img className={styles.image}src="https://picsum.photos/200/300?random=1"></img>
+            <h2 className={styles.titleC}>My Text 1</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
           </div>
           <div className={styles.card}>
-            <h2>Card 3</h2>
-            <p>Conteúdo do Card 3</p>
+            <img className={styles.image}src="https://picsum.photos/200/300?random=1"></img>
+            <h2 className={styles.titleC}>My Text 2</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
           </div>
           <div className={styles.card}>
-            <h2>Card 4</h2>
-            <p>Conteúdo do Card 4</p>
+            <img className={styles.image}src="https://picsum.photos/200/300?random=1"></img>
+            <h2 className={styles.titleC}>My Text 3</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
           </div>
           <div className={styles.card}>
-            <h2>Card 5</h2>
-            <p>Conteúdo do Card 5</p>
+            <img className={styles.image}src="https://picsum.photos/200/300?random=1"></img>
+            <h2 className={styles.titleC}>My Text 4</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
           </div>
         </div>
       </main>
