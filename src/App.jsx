9d38a170129a852b769bb32e97a8cc8ddd2d2f35
ArturLimaMelo <151844637+ArturLimaMelo@users.noreply.github.com>
2025-06-13 17:37:41 +0000

    desafio4 main

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import "./styles/theme.css";
 import "./styles/global.css";
 import { MyTextList } from "./components/MyTextList";
-import { MyGrid } from "./components/MyGrid";
+import { Header } from "./components/Header";
+import { Main } from "./components/Main";
+import { Footer } from "./components/Footer";
 
 export default function App() {
   return (
     <>
-      <MyGrid />
+      <div className="container">
+        <Header />
+        <Main />
+        <Footer />
+      </div>
     </>
   );
 }
