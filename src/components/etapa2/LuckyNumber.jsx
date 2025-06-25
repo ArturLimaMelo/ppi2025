34d6e9a6fcd9a50fe428ffb4e2b1cc5210756862
ArturLimaMelo <151844637+ArturLimaMelo@users.noreply.github.com>
2025-06-25 20:21:33 +0000

    17:21

--- src/components/etapa2/LuckyNumber.jsx
+++ src/components/etapa2/LuckyNumber.jsx
@@ -1,9 +1,16 @@
 import styles from './LuckyNumber.module.css';
+import { useState } from 'react';
 
 export function LuckyNumber() {
+    const [luckyNumber, setLuckyNumber] = useState(0); 
+
+    function handleClick() {
+        setLuckyNumber(Math.floor(Math.random() * 40) + 1);
+    }
+    
     return <div className ={styles.container}>
-            <h1> Lucky Number: = ?</h1>
-            <button className={styles.button}>
+            <h1> Lucky Number: = {luckyNumber}</h1>
+            <button className={styles.button} onClick={handleClick}>
                 I'm Feeling Lucky Today!
             </button>
         </div>
