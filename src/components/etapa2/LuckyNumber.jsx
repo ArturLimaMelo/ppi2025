4e8e4025e2e5dc1b71b110362c85eee8bb8ef165
ArturLimaMelo <151844637+ArturLimaMelo@users.noreply.github.com>
2025-06-26 16:23:02 +0000

    desafio_final

--- src/components/etapa2/LuckyNumber.jsx
+++ src/components/etapa2/LuckyNumber.jsx
@@ -4,12 +4,14 @@ import { useState } from 'react';
 export function LuckyNumber() {
     const [luckyNumber, setLuckyNumber] = useState(0); 
     var [array, setArray] = useState(["0, "]);
+    var [msg, setMsg] = useState("");
     function handleClick() {
         var result = Math.floor(Math.random() * 40) + 1;
         setLuckyNumber(result);
         if (array.includes((result + ", "))) {
-            setArray("Já sorteado!");
+            setMsg("Este número já foi sorteado!");
         } else {
+            setMsg("");
             setArray(0 + ", ");
             array.push(result+ ", ");
             setArray(array);
@@ -25,5 +27,6 @@ export function LuckyNumber() {
                 I'm Feeling Lucky Today!
             </button>
             <h1>{array}</h1>
+            <h1>{msg}</h1>
         </div>
 }
