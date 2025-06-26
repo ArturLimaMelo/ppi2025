65f01d85d177a648ce3516022e6574f1d8a1748b
ArturLimaMelo <151844637+ArturLimaMelo@users.noreply.github.com>
2025-06-26 16:22:01 +0000

    receba

--- src/components/etapa2/LuckyNumber.jsx
+++ src/components/etapa2/LuckyNumber.jsx
@@ -3,24 +3,27 @@ import { useState } from 'react';
 
 export function LuckyNumber() {
     const [luckyNumber, setLuckyNumber] = useState(0); 
-
+    var [array, setArray] = useState(["0, "]);
     function handleClick() {
-        setLuckyNumber(Math.floor(Math.random() * 40) + 1);
+        var result = Math.floor(Math.random() * 40) + 1;
+        setLuckyNumber(result);
+        if (array.includes((result + ", "))) {
+            setArray("Já sorteado!");
+        } else {
+            setArray(0 + ", ");
+            array.push(result+ ", ");
+            setArray(array);
+        }
+        
     }
     
     return <div className ={styles.container}>
 
-            {luckyNumber === 0 ? (
-                <h1>Lucky Number 🎲</h1>
-            ) : (
-                <h1> Lucky Number: = {luckyNumber}</h1>
-            )
-
-            }
-            
+            {luckyNumber === 0 ? (<h1>Lucky Number 🎲</h1>) : (<h1> Lucky Number: = {luckyNumber}</h1>)}
             
             <button className={styles.button} onClick={handleClick}>
                 I'm Feeling Lucky Today!
             </button>
+            <h1>{array}</h1>
         </div>
 }
